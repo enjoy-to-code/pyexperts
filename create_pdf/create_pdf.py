4bb1949dd590ef163ad38262b107a35457d99bab
Harold Kasperink <harold.kasperink@gmail.com>
2021-06-18 10:08:58 +0200

    Add files via upload

---
+++ create_pdf/create_pdf.py
@@ -0,0 +1,11 @@
+from fpdf import FPDF
+
+pdf = FPDF()
+pdf.add_page()
+pdf.set_font("Arial", size=12)
+pdf.cell(200, 10, 
+         txt="Welcome to Python PDF Creator", 
+         ln=1, align="L")
+pdf.output("python.pdf")
+
+
